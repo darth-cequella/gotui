e2e9cdb503c97418b4a594057d59443f525a5c85
darth-cequella <cequelldev@gmail.com>
2016-01-25 01:07:27 -0300

    Adding the primitives of screen size

--- gotui.go
+++ gotui.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"os"
+	"syscall"
+	"unsafe"
 	"fmt"
-	"os/exec"
-	"bytes"
-	"log"
 	
-	//"gotui/widget"
+	"gotui/widget"
 )
 
+const (
+    _TIOCGWINSZ = 0x5413 // On OSX use 1074295912. Thanks zeebo
+)
+
+type WINSIZE struct {
+    Row    uint16
+    Col    uint16
+    Xpixel uint16
+    Ypixel uint16
+}
+
+//What the fuck?
+func GetWinsize() (*WINSIZE, error) {
+    ws := new(WINSIZE)
+
+    r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+        uintptr(syscall.Stdin),
+        uintptr(_TIOCGWINSZ),
+        uintptr(unsafe.Pointer(ws)),
+    )
+
+    if int(r1) == -1 {
+        return nil, os.NewSyscallError("GetWinsize", errno)
+    }
+    return ws, nil
+}
+
 func main() {
-	cmd := exec.Command ("echo", "-e lines\ncols | tput -S")
+	win,_ := GetWinsize()
 	
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	title := "Ola mundo"
+	space := (int(win.Col) - len(title))/2
+	var test string
+	for i:=0; i<space; i++ { test+=" " }
+	test += title
+	for i:=0; i<space; i++ { test+=" " }
 	
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s \n", out.String())
-	
-	//fmt.Println( widget.FramedLabel("Ola mundo", widget.NORMAL, widget.WHITE, widget.CYAN) )
+	fmt.Println( widget.Label(test, widget.NORMAL, widget.WHITE, widget.CYAN) )
 }
