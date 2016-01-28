a1c63e93be5b51d157769af25f46aff4d391667f
darth-cequella <cequelldev@gmail.com>
2016-01-28 12:17:15 -0300

    Adding listener for CTRL-C

--- gotui.go
+++ gotui.go
@@ -7,33 +7,20 @@ import (
     "os/exec"
     "os/signal"
     "syscall"
-	
-	"gotui/widget"
-	"gotui/base"
 )
 
 func cleanup() {
+	err := exec.Command("stty", "-F", "/dev/tty", "sane").Run()
+	
+	if err != nil{
+		fmt.Println("Não executou")
+		log.Fatal(err)
+	}
+	
 	fmt.Println("Ola mundo!")
-	//exec.Command("stty", "sane").Run()
 }
 
 func main() {
-	screen,err := base.GetScreenCanvas()
-	if err != nil { log.Fatal(err) }
-	
-	var label widget.Label
-	
-	test := base.Canvas{0,0, screen.W/2,screen.H/2}
-	
-	label.SetContent("Testando")
-	label.SetCanvas( test )
-	label.SetForeground(widget.WHITE)
-	label.SetBackground(widget.BLUE)
-	label.SetStyle(widget.BOLD)
-	
-	fmt.Println( label.Draw() )
-	
-	//------------------------------------------------------------------
 	channel := make(chan os.Signal, 1)
     signal.Notify(channel, os.Interrupt)
     signal.Notify(channel, syscall.SIGTERM)
@@ -44,11 +31,12 @@ func main() {
     }()
     
     exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run() 
 	
 	var b []byte = make([]byte, 1)
 	for {
 		os.Stdin.Read(b)
-        fmt.Println(string(b))
-	}
+        fmt.Print(string(b))
+	} 
+
 }
